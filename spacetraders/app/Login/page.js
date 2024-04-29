deac9fcadeb7e69694c8f852af82564880e30f31
Tanner Coker <tbcoker13@gmail.com>
2024-04-29 17:08:00 -0500

    Added ability to switch the cardcontent to render the components for either login or register

--- spacetraders/app/Login/page.js
+++ spacetraders/app/Login/page.js
@@ -1,23 +1,54 @@
-import React from 'react'
-import Link from 'next/link';
+'use client'
 
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import React, { useState } from 'react'
+
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+
 
 const Login = () => {
-  return (
-    <div className='flex h-screen justify-center items-center'>
-        <Card className='w-[350px] border-customSecondary'>
-            <CardHeader className='items-center'>
-                <CardTitle className="text-2xl text-customPrimary">Space Traders</CardTitle>
-            </CardHeader>
-            <CardContent className='flex flex-col items-center'>
-                <button type='button' className='my-5 w-32'>Login</button>
-                <div className='flex w-full border-t border-customAccent'></div>
-                <button className='my-5 w-32'>Register</button>
-            </CardContent>
-        </Card>
-    </div>
-  )
+    const [displayedCard, setDisplayedCard] = useState();
+    
+    const switchView = (view) => {
+        setDisplayedCard(view);
+    }
+
+    const cardContent = () => {
+        switch(displayedCard) {
+            case 'login':
+                return (
+                    <div className=''>
+                        <button type='button' onClick={() => {switchView('')}}>back</button>
+                    </div>
+                )
+            case 'register':
+                return (
+                    <div className=''>
+                        <button type='button' onClick={() => {switchView('')}}>back</button>
+                    </div>
+                )
+            default: 
+                return (
+                    <div className='flex flex-col w-full items-center'>
+                        <button type='button' className='my-5 w-32' onClick={() => {switchView('login')}}>Login</button>
+                        <div className='flex w-full border-t border-customAccent'></div>
+                        <button className='my-5 w-32' onClick={() => {switchView('register')}}>Register</button>
+                    </div>
+                )
+        }
+    }
+
+    return (
+        <div className='flex h-screen justify-center items-center'>
+            <Card className='w-[350px] border-customSecondary'>
+                <CardHeader className='items-center'>
+                    <CardTitle className="text-2xl text-customPrimary">Space Traders</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    {cardContent()}
+                </CardContent>
+            </Card>
+        </div>
+    )
 }
 
 export default Login
